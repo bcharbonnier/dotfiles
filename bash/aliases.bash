b9549b85ef75a3b44b64da8d93467ad9c6202fc9
bcharbonnier <benoit.charbonnier@gmail.com>
2019-12-09 11:08:03 +0100

    feat: toggle desktop icons actions present/present\!

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -13,11 +13,15 @@ alias flush='dscacheutil -flushcache'
 alias dsclean='find . -type f -name .DS_Store -delete'
 
 # Start a local webserver
-alias serve="python -m SimpleHTTPServer"
+#alias serve="python -m SimpleHTTPServer"
 
 # Show/hide hidden files in Finder
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Show/hide desktop icons
+alias present="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias present!="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
 
 # reload config
 alias reload!='source ~/.bash_profile'
