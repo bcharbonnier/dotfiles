f3da38a0d78ec4c3ffdcd27aa59add05a0ea02c0
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2020-01-22 14:37:41 +0100

    fix: forces Terminal.app to open new tabs in current directory

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -73,5 +73,17 @@ set_prompt () {
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
+
+  # http://superuser.com/a/315029/4952
+  # Set Apple Terminal.app to resume directory... still necessary 2020-01-22
+  if [[ $TERM_PROGRAM == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] {
+    function chpwd {
+      local SEARCH=' '
+      local REPLACE='%20'
+      local PWD_URL="file://$HOSTNAME${PWD//$SEARCH/$REPLACE}"
+      printf '\e]7;%s\a' "$PWD_URL"
+    }
+    chpwd
+  }
   set_prompt
 }
